acf5c8bdec058b63cb34411e301b4372e02ca4a2
Romain Boisselle <romain@boisselle.dev>
2019-12-02 13:06:25 +0100

    kodein usage

--- src/commonMain/kotlin/com/mybusiness/di/CommonInjector.kt
+++ src/commonMain/kotlin/com/mybusiness/di/CommonInjector.kt
@@ -4,16 +4,23 @@ import com.mybusiness.api.ContactApi
 import com.mybusiness.presentation.ContactCreationUpdatePresenter
 import com.mybusiness.presentation.ContactDetailPresenter
 import com.mybusiness.presentation.ContactListPresenter
+import org.kodein.di.Kodein
+import org.kodein.di.direct
+import org.kodein.di.erased.instance
 import kotlin.native.concurrent.ThreadLocal
 
 @ThreadLocal
 object CommonInjector {
-    //    API
-    private val api: ContactApi by lazy {
-        ContactApi()
+    val kodein = Kodein.lazy {
+        import(addressBookModule)
     }
+
     //    Presenters
-    fun contactListPresenter() = ContactListPresenter(api)
-    fun contactDetailPresenter() = ContactDetailPresenter(api)
-    fun contactCreationUpdatePresenter() = ContactCreationUpdatePresenter(api)
+    fun contactListPresenter(): ContactListPresenter {
+        val presenter = kodein.direct.instance<ContactListPresenter>()
+        println("ContactListPresenter instance: $presenter")
+        return presenter
+    }
+    fun contactDetailPresenter() = kodein.direct.instance<ContactDetailPresenter>()
+    fun contactCreationUpdatePresenter() = kodein.direct.instance<ContactCreationUpdatePresenter>()
 }
